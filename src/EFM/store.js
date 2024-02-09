c690849f1324c575e0c5f4875d6017cba67bd2a4
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-09 17:40:13 +0100

    exercice 2 donne

--- src/EFM/store.js
+++ src/EFM/store.js
@@ -1,1 +1,16 @@
 import {createStore} from 'redux'
+
+const initialState={}
+const reducer=(state=initialState,{type,payload})=>{
+    switch(type){
+        case 'add' :
+            return {}
+        break
+        default :
+        return state ;
+    }
+
+}
+
+const Store=createStore(reducer);
+export default Store
