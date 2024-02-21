6719d6316edc0b9c7615c3023032e2e62e546a2a
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-21 20:48:23 +0100

    add style to home page

--- src/project/components/actors.jsx
+++ src/project/components/actors.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Actors = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [actors, setActors] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=0269e1f69afd6ff169f8a6a2d9f0dc4d')
+            .then((response) => setActors(response.data['results']));
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="row">
+                {actors.map((e, k) => (
+                    <div className="col-md-4 mb-4" key={k}>
+                        <div className="card">
+                            <img src={'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/' + e.poster_path} className="card-img-top" alt={e.title} />
+                            <div className="card-body">
+                                <h5 className="card-title">{e.title}</h5>
+                                <button variant="primary">Add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Actors
+export default Actors;
--- src/project/components/header.jsx
+++ src/project/components/header.jsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { Routes, Route, Link } from 'react-router-dom'
-import Act from './actors'
-
-
-
+import { Routes, Route, Link } from 'react-router-dom';
+import Actors from './actors';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 export default function Header() {
-    
     return (
         <>
-        <nav>
-                <ul>
-                    <Link to='/'>Home</Link>
-                    <Link to='/actors'>Actors</Link>
-                </ul>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">Home</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/actors">Actors</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
-            
-        
-        <Routes>
+
+            <Routes>
                 <Route path="/" />
-                <Route path="/actors" element={<Act/>}/>
-        </Routes>
+                <Route path="/actors" element={<Actors />} />
+            </Routes>
         </>
     )
-
 }
