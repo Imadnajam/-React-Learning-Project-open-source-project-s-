cc70a34b3ac527b7854a385a58dbb1ac7f8b70f5
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-21 20:20:20 +0100

    add actors page

---
+++ src/project/components/header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom'
+import Act from './actors'
+
+
+
+
+export default function Header() {
+    
+    return (
+        <>
+        <nav>
+                <ul>
+                    <Link to='/'>Home</Link>
+                    <Link to='/actors'>Actors</Link>
+                </ul>
+            </nav>
+            
+        
+        <Routes>
+                <Route path="/" />
+                <Route path="/actor" element={Act}/>
+        </Routes>
+        </>
+    )
+
+}
