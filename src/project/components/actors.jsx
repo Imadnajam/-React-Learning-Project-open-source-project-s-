4b6b2b72d42a119f8b90ea48e45cc3bf5e4ef761
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-23 19:41:24 +0100

    add actors page 2

--- src/project/components/actors.jsx
+++ src/project/components/actors.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Actors = () => {
+    const [artist, setArtist] = useState([]);
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/person/popular?api_key=0269e1f69afd6ff169f8a6a2d9f0dc4d`)
+    },[])
   return (
     <div>
       
