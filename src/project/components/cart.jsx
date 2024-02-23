9db738744f7ff50061173c710c36bda44968a8ea
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-02-23 19:56:32 +0100

    add actors page 6

--- src/project/components/cart.jsx
+++ src/project/components/cart.jsx
@@ -3,33 +3,55 @@ import { useDispatch, useSelector } from 'react-redux'
 
 const Cart = () => {
     const items = useSelector((state) => state.Films);
+    const acto = useSelector((state) => state.Actors);
     const dispatch = useDispatch();
     function removeItem(item) {
-        dispatch({ type:'removeItem',payload:item})
+        dispatch({ type: 'removeItem', payload: item })
     }
-  return (
-      <div>
-          <div className="container">
-              <div className="row">
-          {
-              items.map((e, k) => (
-                  <>
-                      <div className="col-md-4 mb-4" key={k}>
-                          <div className="card">
-                              <img src={'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/' + e.poster_path} className="card-img-top" alt={e.title} />
-                              <div className="card-body">
-                                  <h5 className="card-title">{e.title}</h5>
-                                  <button className='btn btn-danger' onClick={() => removeItem(e)}>remove</button>
-                              </div>
-                          </div>
-                      </div>
-                  </>
-              ))
-                  }
-              </div>
-              </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <h2>Films</h2>
+                    {
+                        items.map((e, k) => (
+                            <>
+                                <div className="col-md-4 mb-4" key={k}>
+                                    <div className="card">
+                                        <img src={'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/' + e.poster_path} className="card-img-top" alt={e.title} />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{e.title}</h5>
+                                            <button className='btn btn-danger' onClick={() => removeItem(e)}>remove</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ))
+                    }
+                </div>
+                <h2>Actors</h2>
+                <div className="row">
+                    {
+                        acto.map((e, k) => (
+                            <>
+                                <div className="col-md-4 mb-4" key={k}>
+                                    <div className="card">
+                                        <img src={'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/' + e.profile_path} className="card-img-top" alt={e.title} />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{e.name}</h5>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ))
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Cart
