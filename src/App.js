80e3265c36f3e966be32af96193f5d5e26e48190
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-06-16 12:13:49 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import Hi from './practis/Stars';
-
-
+import { React, useState } from 'react';
+import axios from 'axios';
+import StarsC from './practis/Stars';
+import StarsFC from './practis/FindStart';
+import { Routes, Route, Link } from 'react-router-dom';
+// 
 export default function App() {
+  const [stars, setStars] = useState([]);
+  const fetApi =async() =>{
+    const response = await axios.get("https://api.themoviedb.org/3/person/popular?api_key=0269e1f69afd6ff169f8a6a2d9f0dc4d");
+    setStars(response.data.results)
+  }
+  fetApi()
+   
   return (
-
     <>
-      <Hi />
+      <Link to={"/stars"}>stars</Link>
+      <Link to={"/findS"}>Find Stars By Name</Link>
+
+    <Routes>
+      <Route path='/stars' element={<StarsC stars={stars} />} />
+      <Route path='/findS' element={<StarsFC stars={stars} />} />
+      
+      </Routes>
     </>
   )
  
--- src/practis/Stars.jsx
+++ src/practis/Stars.jsx
@@ -2,25 +2,14 @@ import { React, useEffect, useState } from "react";
 import axios from "axios";
 
 
-const Stars = () => {
+const Stars = ({stars}) => {
     
-    const [starts, setStars] = useState([]);
-    const [page,setPage]= useState(1)
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await axios.get(`https://api.themoviedb.org/3/person/popular?api_key=0269e1f69afd6ff169f8a6a2d9f0dc4d&page=${page}`)
-            setStars(response.data.results);
-        }
-        fetchData();
-    }, [page])
-   
 
     
 
     return (
         <>
-            {starts.map((star) => (
+            {stars.map((star) => (
                 <>
                     
                 <li key={star.id}>{star.name}</li>
@@ -28,7 +17,6 @@ const Stars = () => {
            </>
             ))}
 
-            <button onClick={()=>setPage(page+1)}>Next</button>
         </>
     )
 
