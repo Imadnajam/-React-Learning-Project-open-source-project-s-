735406bda8a15f2efb12450fc55d3177bd8b7022
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-06-22 12:52:11 +0100

    In the NExt day 2

--- src/practis/CRUD.jsx
+++ src/practis/CRUD.jsx
@@ -1,11 +1,33 @@
+import { React, useState } from 'react';
+
+const CRUD = ({ stars }) => {
+ 
 
 
-const CRUD = ({stars}) => {
-
+    function removeStar(item) {
+        
+        stars.filter((e) => e !== item);
+        
+     }
 
     return (
         <div>
             <h2> In the NExt day</h2>
+
+            {
+                stars.length > 0 ? (
+                    stars.map((e) => (
+                        <>
+                            <li key={e.id}>{e.name}</li>
+                            <img src={'https://media.themoviedb.org/t/p/w300_and_h450_bestv2/' + e.profile_path} />
+                            <button onClick={()=>removeStar(e)}>Remove</button>
+
+                        </>
+                    ))
+                ): (
+                        <h3>We dont have any stars</h3>
+                )
+            }
 
 
         </div>
