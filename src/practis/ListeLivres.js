6797bc2a70c4a882eac60fb679f762b333cdee56
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-06-23 21:12:38 +0100

    Tp donne 100%

--- src/practis/ListeLivres.js
+++ src/practis/ListeLivres.js
@@ -1,14 +1,23 @@
-
+import { useDispatch } from 'react-redux'
 const ListeLivre = ({ Livresd }) => {
-    
 
+    const Dispatch = useDispatch();
+    function delteOne(item) {
+        Dispatch({ type: 'removeLivre', payload: item })
+
+
+    }
     return (
         <div>
 
             {
                 Livresd && Livresd.length > 0 ? (
                     Livresd.map((e) => (
-                        <li key={e.Livre}>Livre: {e.Livre} / Auteur: {e.Auteur}</li>
+                        <div>
+                            <li key={e.Livre}>Livre: {e.Livre} / Auteur: {e.Auteur}</li>
+                            <button onClick={()=>delteOne(e)}>Remove</button>
+                        </div>
+
                     ))
                 ) : (
                     <h3>We don't have any books</h3>
--- src/practis/app/Reducer.js
+++ src/practis/app/Reducer.js
@@ -11,6 +11,8 @@ const reducer = (State = initialeState, { type, payload })=> {
     switch (type) {
         case "AddNewLivre":
             return { ...State, Data: [...State.Data, payload] };
+        case "removeLivre":
+            return { Data: State.Data.filter((e)=>e!==payload)}
          default:
             return State;
     }
